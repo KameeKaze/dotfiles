e6bf78a7eacd1d5ee9dc517497b7e396336e2a0d
Emma Gubucz <k4m33k4z3@protonmail.com>
2024-01-28 13:44:57 +0100

    update dwm config

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -1,14 +1,15 @@
+#include <X11/XF86keysym.h>
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
 
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int gappx     = 12;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hack Nerd Font:size=12" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "nerdfont:size=12" };
+static const char dmenufont[]       = "nerdfont:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -23,6 +24,8 @@ static const char *colors[][3]      = {
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -41,8 +44,8 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[]",      tile },    /* first entry is default */
+	{ "<>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
 
@@ -62,13 +65,27 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_purple, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
-static const char *screenshoot[] = { "scrot", "-s" };
+static const char *slock[] = { "slock", NULL };
+static const char *volumeup[]    = { "amixer", "set", "Master", "2%+", NULL };
+static const char *volumedown[]  = { "amixer", "set", "Master", "2%-", NULL };
+static const char *mutevol[]     = { "amixer", "set", "Master", "toggle", NULL };
+
+static const char *brightnessup[]     = { "xbacklight", "-inc", "5", NULL };
+static const char *brightnessdown[]   = { "xbacklight", "-dec", "5", NULL };
+
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ 0,                            PrintScreen, spawn,         {.v = screenshoot } },
+	{ 0,                            XF86XK_AudioLowerVolume,  spawn,  {.v = volumedown } },
+	{ 0,                            XF86XK_AudioRaiseVolume,  spawn,  {.v = volumeup } },
+	{ 0,                            XF86XK_AudioMute,         spawn,  {.v = mutevol } },
+	{ 0,                            XF86XK_MonBrightnessUp,   spawn,  {.v = brightnessup } },
+	{ 0,                            XF86XK_MonBrightnessDown, spawn,  {.v = brightnessdown } },
+	{ MODKEY,                       PrintScreen, spawn,        SHCMD("maim -m 10 | tee ~/screenshots/screenshot_$(date '+%Y%m%d_%H%M%S').png | xclip -selection clipboard -t image/png") },
+	{ 0,                            PrintScreen, spawn,        SHCMD("maim  -m 10 -s | tee ~/screenshots/screenshot_$(date '+%Y%m%d_%H%M%S').png | xclip -selection clipboard -t image/png") },
 	{ MODKEY|ShiftMask,             XK_x,      quit,      	   {0} },
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_x,      spawn,          {.v = slock } },
 	{ MODKEY,                  		XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
